e66cf338b7ef46292352cb146675575397740b97
Joshua Benjamin <117050813+chijindumu@users.noreply.github.com>
2026-02-24 01:09:16 +0100

    Update debounnce_fsmd.v

--- VGA_pong_1/debounnce_fsmd.v
+++ VGA_pong_1/debounnce_fsmd.v
@@ -1,4 +1,25 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/22/2026 01:28:47 PM
+// Design Name: 
+// Module Name: debouncer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module debounce(
     input clk, reset,
     input wire btn,
