57033a18b2a661e356951718f8ba66292e5f48cc
Joshua Benjamin <117050813+chijindumu@users.noreply.github.com>
2026-02-24 01:07:17 +0100

    Update vga_controller.v

--- VGA_pong_1/vga_controller.v
+++ VGA_pong_1/vga_controller.v
@@ -1,4 +1,25 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/22/2026 04:26:43 PM
+// Design Name: 
+// Module Name: vga_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module vga_controller(
     input clk_100Mhz,
     input reset,
